5f132241c6e6893f66ab2a853f7765136dfcdcc3
Brandon Sturgeon <sturgeonb4@gmail.com>
2020-04-13 05:10:23 -0400

    First working non-greedy solution

--- warmane_coin_gold_value.js
+++ warmane_coin_gold_value.js
@@ -1,89 +1,133 @@
-// TODO: This is a "greedy" solution to the Knapsack problem. What we need is a Knapsack 1/0 imlementation.
+fetch('https://cdn.jsdelivr.net/npm/lodash@4.17.4/lodash.min.js')
+    .then(response => response.text())
+    .then(text => eval(text))
+
+let knapsack = {};
+(function() {
+  this.combiner = function(items, weightfn, valuefn) {
+    // approximation guarantees result >= (1-e) * optimal
+    var _epsilon = 0.01;
+    var _p = _.max(_.map(items,valuefn));
+    var _k = _epsilon * _p / items.length;
+
+    var _memo = (function(){
+      var _mem = {};
+      var _key = function(i, w) {
+        return i + '::' + w;
+      };
+      return {
+        get: function(i, w) {
+          return _mem[_key(i,w)];
+        },
+        put: function(i, w, r) {
+          _mem[_key(i,w)]=r;
+          return r;
+        }
+      };
+    })();
+
+    var _m = function(i, w) {
+
+      i = Math.round(i);
+      w = Math.round(w);
 
 
-const getCartStats = cart => {
-    const coinCosts = cart.map(e => e.coins)
-    const totalCoins = coinCosts.reduce((a,b) => a + b, 0)
+      if (i < 0 || w === 0) {
+        // empty base case
+        return {items: [], totalWeight: 0, totalValue: 0};
+      }
 
-    const goldReceived = cart.map(e => e.gold)
-    const totalGold = goldReceived.reduce((a,b) => a + b, 0)
+      var mm = _memo.get(i,w);
+      if (!_.isUndefined(mm)) {
+        return mm;
+      }
 
-    const ratio = totalGold / totalCoins
-
-    return { totalGold, totalCoins, ratio }
-}
-
-const printCart = cart => {
-    const stats = getCartStats(cart)
-
-    console.log(`Total coins spent: ${stats.totalCoins}`)
-    console.log(`Total gold received: ${stats.totalGold}`)
-    console.log(`Gold / Coin Ratio: ${stats.ratio}`)
-
-    console.log("Items:")
-    cart.forEach(e => {
-        const gold = e.gold
-        const coins = e.coins
-        console.log(`Buy ${gold} gold for ${coins} coins`)
-    })
-}
+      var item = items[i];
+      if (weightfn(item) > w) {
+        //item does not fit, try the next item
+        return _memo.put(i, w, _m(i-1, w));
+      }
+      // this item could fit.
+      // are we better off excluding it?
+      var excluded = _m(i-1, w);
+      // or including it?
+      var included = _m(i-1, w - weightfn(item));
+      if (included.totalValue + Math.floor(valuefn(item)/_k) > excluded.totalValue) {
+        // better off including it
+        // make a copy of the list
+        var i1 = included.items.slice();
+        i1.push(item);
+        return _memo.put(i, w,
+          {items: i1,
+           totalWeight: included.totalWeight + weightfn(item),
+           totalValue: included.totalValue + Math.floor(valuefn(item)/_k)});
+      }
+      //better off excluding it
+      return _memo.put(i,w, excluded);
+    };
+    return {
+      /* one point */
+      one: function(maxweight) {
+        var scaled = _m(items.length - 1, maxweight);
+        return {
+          items: scaled.items,
+          totalWeight: scaled.totalWeight,
+          totalValue: scaled.totalValue * _k
+        };
+      },
+      /* the entire EF */
+      ef: function(maxweight, step) {
+        return _.map(_.range(0, maxweight+1, step), function(weight) {
+          var scaled = _m(items.length - 1, weight);
+          return {
+            items: scaled.items,
+            totalWeight: scaled.totalWeight,
+            totalValue: scaled.totalValue * _k
+          };
+        });
+      }
+    };
+  };
+}).apply(knapsack);
 
 const getListings = () => {
     let dataset = []
     $(".searchItemList").find("tr.odd,tr.even").each((index, element) => {
-        const gold = parseInt($($(element).find("span")[0]).text())
-        const coins = parseInt($($(element).find("span")[1]).text())
-        const ratio = gold / coins
+        const ele = $(element)
+        const spans = ele.find("span")
 
-        dataset.push({ gold, coins, ratio })
+        const value = parseInt($(spans[0]).text())
+        const weight = parseInt($(spans[1]).text())
+
+        dataset.push({ value, weight, element })
     })
-
-    dataset.sort((a,b) => (a.ratio < b.ratio) ? 1 : -1)
 
     return dataset
 }
 
-const bestValueForCoins = (_max, _cart, _listings) => {
-    let max = _max
-    const cart = _cart || []
+const calculateCart = (maxCoins) => {
+    const listings = getListings()
+    const getWeight = x => x.weight
+    const getValue = x => x.value
 
-    if (max <= 1) {
-        return cart
-    }
+    const combiner = knapsack.combiner(listings, getWeight, getValue)
+    const cart = combiner.one(maxCoins)
 
-    const listings = _listings || getListings()
-    let dataset = listings.slice()
-
-    dataset = dataset.filter(listing => listing.coins <= max && !cart.includes(a))
-
-    const next = dataset[0]
-    if (next === undefined) {
-        return cart
-    }
-
-    cart.push(next)
-    max = max - next.coins
-
-    return bestValueForCoins(max, cart, listings)
+    return cart
 }
 
-const getCartForCoins = coins => printCart(bestValueForCoins(coins))
+const hideOtherItems = (except) => {
+    const parent = $(except[0]).parent()
 
-const findBestCoinValue = () => {
-    const values = {}
-    for (var i=3; i < 100; i++) {
-        const cart = bestValueForCoins(i)
+    $(parent).children().each((index, element) => {
+        if(!except.includes(element)) {
+            $(element).css("display", "none")
+        }
+    })
+}
 
-        const coinCosts = cart.map(e => e.coins)
-        const totalCoins = coinCosts.reduce((a,b) => a + b, 0)
-
-        const goldReceived = cart.map(e => e.gold)
-        const totalGold = goldReceived.reduce((a,b) => a + b, 0)
-
-        const ratio = totalGold / totalCoins
-
-        values[i] = {"gold": totalGold, "ratio": ratio}
-    }
-
-    console.log(values)
+const filterForBest = (maxCoins) => {
+    const cart = calculateCart(maxCoins)
+    const elements = _.map(cart.items, i => i.element)
+    hideOtherItems(elements)
 }
